b30a6eae3297b5bd03201e9ec0e28cfc3fb1341d
Taka Kamio <takahiro.1203.k@gmail.com>
2024-01-25 08:05:40 +0900

    create view and modify controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,23 +1,27 @@
 class BookingsController < ApplicationController
   def new
-    @vehicle = Vehicle.find(params[:id])
+    @vehicle = Vehicle.find(params[:vehicle_id])
     @booking = Booking.new
+    @booking.user = current_user
   end
 
   def create
     @vehicle = Vehicle.find(params[:booking][:vehicle_id])
     @booking = Booking.new(booking_params)
     @booking.vehicle = @vehicle
+    @booking.status = 'pending'
+    @booking.user = current_user
 
     if @booking.save
-      redirect_to @booking, notice: 'Booking was successfully created.'
+      redirect_to @vehicle, notice: 'Booking was successfully created.'
     else
-      render :new
+      Rails.logger.info(@booking.errors.full_messages)
+      render :new, notice: 'Booking failed.'
     end
   end
 
   private
   def booking_params
-    params.require(:booking).permit(:start_date, :end_date, :status, :user_id, :vehicle_id)
+    params.require(:booking).permit(:start_date, :end_date, :status, :vehicle_id) #add :user_id later
   end
 end
