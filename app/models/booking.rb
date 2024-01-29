7e17b1871cc66b51e22fbe5c63cfa68fbcd6293a
Funayama Miho <funamiho1011@gmail.com>
2024-01-29 19:23:03 +0900

    installed date_validator gem

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -7,7 +7,7 @@ class Booking < ApplicationRecord
   validates :vehicle, presence: true
   validates :status, presence: true
   validates :start_date, presence: true
-  validates :end_date, presence: true
+  validates :end_date, presence: true, date: { after_or_equal_to:  :start_date}
 
   enum status: [:pending, :approved, :declined, :cancelled]
 end
